9cdfac97493d62d4590e38af654bd54297d3be32
Chad Moravec <moravec@colorado.edu>
2021-01-29 16:23:01 -0700

    Production routes

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-  API_ENDPOINT:
-    "https://cors-anywhere.herokuapp.com/https://gratitudes-server.herokuapp.com/api",
+  API_ENDPOINT: "https://gratitudes-server.herokuapp.com/api",
   TOKEN_KEY: "gratitude-client-auth-token",
 };
