adda70e48fdf36f7f7c6478d38f1b5c2e3e57a26
Chad Moravec <moravec@colorado.edu>
2021-02-01 15:19:00 -0700

    Fixed routes

--- src/config.js
+++ src/config.js
@@ -1,4 +1,11 @@
+// export default {
+//   API_ENDPOINT: "https://gratitudes-server.herokuapp.com/api",
+//   TOKEN_KEY: "gratitude-client-auth-token",
+// };
+
 export default {
-  API_ENDPOINT: "https://gratitudes-server.herokuapp.com/api",
-  TOKEN_KEY: "gratitude-client-auth-token",
+  API_ENDPOINT:
+    process.env.REACT_APP_API_ENDPOINT ||
+    `https://gratitudes-server.herokuapp.com/api`,
+  TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY,
 };
